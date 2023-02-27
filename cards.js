a46677bb6d4d3f5f53134e930d11662ac8c84ac8
orapple89 <125343651+orapple89@users.noreply.github.com>
2023-02-27 04:35:21 +0000

    add image to array

--- cards.js
+++ cards.js
@@ -15,6 +15,7 @@ var cards = {
     cardNum: 1,
     cardName: 'Chopsticks',
     cardStatus: 1, //card status 1=deck 2=hand 3=played 4=discard
+    smallCard: /img/Chopsticks.png,
     
    },
      ]
