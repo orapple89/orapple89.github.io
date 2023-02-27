1f3c68f4ada7afc4f4b1141cea104ea7a2092be7
orapple89 <125343651+orapple89@users.noreply.github.com>
2023-02-27 04:31:43 +0000

    establish deck and palyer arrays

---
+++ cards.js
@@ -0,0 +1,21 @@
+var players = {
+    "members" : [
+    {
+
+
+
+    },
+]
+};
+
+
+var cards = {
+    "members" : [
+    {
+    cardNum: 1,
+    cardName: 'Chopsticks',
+    cardStatus: 1, //card status 1=deck 2=hand 3=played 4=discard
+    
+   },
+     ]
+   };
