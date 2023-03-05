fcea435cc5ce9b4b9b341024433c2981255aff4e
orapple89 <125343651+orapple89@users.noreply.github.com>
2023-03-05 20:33:34 +0000

    socket IO test

--- app.js
+++ app.js
@@ -1,17 +1,44 @@
+var express = require('express'),
+    app = express(),
+    http = require('http'),
+    socketIO = require('socket.io'),
+    server, io;
+
+app.get('/', function (req, res) {
+res.sendFile(__dirname + '/index.html');
+});
+
+server = http.Server(app);
+server.listen(5000);
+
+io = socketIO(server);
+
+io.on('connection', function (socket) {
+  socket.emit('greeting-from-server', {
+      greeting: 'Hello Client'
+  });
+  socket.on('greeting-from-client', function (message) {
+    console.log(message);
+  });
+});
+
+/*
 const express = require('express');
 const app = express();
 const port = 3000;
 
-//const httpServer = require("http").createServer(app);
-//const options = { /* ... */ };
-/*
-const io = require("socket.io")(httpServer, options);
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const httpServer = createServer();
+//const io = new Server(httpServer, {options});
 
 io.on("connection", socket => {
   // either with send()
   socket.send("Hello!");
 
-  // or with emit() and custom event names
+
+// or with emit() and custom event names
   socket.emit("greetings", "Hey!", { "ms": "jane" }, Buffer.from([4, 3, 3, 1]));
 
   // handle the event sent with socket.send()
@@ -26,12 +53,11 @@ io.on("connection", socket => {
 });
 
 httpServer.listen(3000);
-
-
-
-WARNING !!! app.listen(3000); will not work here, as it creates a new HTTP server
 */
 
+
+//WARNING !!! app.listen(3000); will not work here, as it creates a new HTTP server
+/*
 //****express test****
 app.get('/', (req,res) => {
     res.send('hello world')
@@ -40,6 +66,8 @@ app.get('/', (req,res) => {
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   })
-
+*/
+ 
+  
 //***node test***
 //console.log("Node is installed xxxnode");
