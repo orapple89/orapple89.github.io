58222a493e76a0e37bcc80322c2e2a8ce54b19fc
orapple89 <125343651+orapple89@users.noreply.github.com>
2023-03-06 04:33:05 +0000

    chat emit

--- app.js
+++ app.js
@@ -15,9 +15,13 @@ io.on('connection', (socket) => {
   socket.on('chat message', (msg) => {
     console.log('message: ' + msg);
   });
-  
+
   socket.on('disconnect', () => {
     console.log('user disconnected');
+  });
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
   });
 
 });
