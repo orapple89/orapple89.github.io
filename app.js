7827381f51ce2b5e0b6f4c92e73d4ac169619f21
orapple89 <125343651+orapple89@users.noreply.github.com>
2023-03-06 04:14:58 +0000

    chat app server

--- app.js
+++ app.js
@@ -1,62 +1,16 @@
-var express = require('express'),
-    app = express(),
-    http = require('http'),
-    socketIO = require('socket.io'),
-    server, io;
-
-app.get('/', function (req, res) {
-res.sendFile(__dirname + '/index.html');
-});
-
-server = http.Server(app);
-server.listen(5000);
-
-io = socketIO(server);
-
-io.on('connection', function (socket) {
-  socket.emit('greeting-from-server', {
-      greeting: 'Hello Client'
-  });
-  socket.on('greeting-from-client', function (message) {
-    console.log(message);
-  });
-});
-
-/*
 const express = require('express');
 const app = express();
-const port = 3000;
+const http = require('http');
+const server = http.createServer(app);
 
-const { createServer } = require("http");
-const { Server } = require("socket.io");
-
-const httpServer = createServer();
-//const io = new Server(httpServer, {options});
-
-io.on("connection", socket => {
-  // either with send()
-  socket.send("Hello!");
-
-
-// or with emit() and custom event names
-  socket.emit("greetings", "Hey!", { "ms": "jane" }, Buffer.from([4, 3, 3, 1]));
-
-  // handle the event sent with socket.send()
-  socket.on("message", (data) => {
-    console.log(data);
-  });
-
-  // handle the event sent with socket.emit()
-  socket.on("salutations", (elem1, elem2, elem3) => {
-    console.log(elem1, elem2, elem3);
-  });
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>');
 });
 
-httpServer.listen(3000);
-*/
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
 
-
-//WARNING !!! app.listen(3000); will not work here, as it creates a new HTTP server
 /*
 //****express test****
 app.get('/', (req,res) => {
