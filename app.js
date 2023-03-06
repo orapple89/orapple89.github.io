e051a7969eceac947d3b6162c92eb7c6dbf822f9
orapple89 <125343651+orapple89@users.noreply.github.com>
2023-03-06 06:43:37 +0000

    csss title page

--- app.js
+++ app.js
@@ -30,17 +30,3 @@ server.listen(3000, () => {
   console.log('listening on *:3000');
 });
 
-/*
-//****express test****
-app.get('/', (req,res) => {
-    res.send('hello world')
-})
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
-*/
- 
-  
-//***node test***
-//console.log("Node is installed xxxnode");
